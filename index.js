5375d3cf81bb7b3280dfd488a2b76fecb07199f1
Timothy Regan <tregan@pillartechnology.com>
2019-02-01 16:06:34 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+const initTable = "ranged"
+var app1 = new Vue({
+    el: '#craft-app',
+    data: {
+        price: 0.0,
+        difficulty: "",
+        table:initTable,
+        rows: tables[initTable],
+        tableNames:Object.keys(tables),
+        tables:tables,
+        improvements: [],
+        totalPrice: 0,
+        totalDifficulty: ""
+    },
+    methods: {
+        addImprovement: function (type, effect) {
+            this.improvements.push({ type: type, effect: effect })
+        },
+        removeImprovement: function (index) {
+            this.improvements.splice(index, 1)
+        }
+    },
+    beforeUpdate: function () {
+        var typeMap = {
+            "simple": { difficulty: "s", costMod: 0.25 },
+            "intermediate": { difficulty: "ss", costMod: 0.25 },
+            "advanced": { difficulty: "d", costMod: 0.25 },
+            "masterwork": { difficulty: "ds", costMod: 1.00 },
+        }
+        const price = parseFloat(this.price)
+        var totalDifficulty = this.difficulty
+        var totalPrice = price
+        this.improvements.forEach(function (improvement) {
+            totalDifficulty += typeMap[improvement.type].difficulty
+            improvement.diff = typeMap[improvement.type].difficulty
+            totalPrice += price * typeMap[improvement.type].costMod
+            improvement.cost = price * typeMap[improvement.type].costMod
+        })
+
+        var difficultyChars = totalDifficulty.split("")
+        for (let index = 0; index < Math.floor(this.improvements.length/3); index++) {
+            const dieIndex = difficultyChars.indexOf("d");
+            if (dieIndex >= 0) {
+                difficultyChars[dieIndex] = "c";
+            } else {
+                difficultyChars.push('d')
+            }
+        }
+
+        difficultyChars.sort()
+
+        this.totalDifficulty = difficultyChars.join("")
+        this.totalPrice = totalPrice
+        this.rows = this.tables[this.table]
+    }
+})
---
+++ tables.js
@@ -0,0 +1,270 @@
+tables = {
+  melee: [
+    {
+      symbols: "ADV", type: "simple", effects: [
+        {
+          name: "Two-Handed",
+          value: ""
+        },
+        {
+          name: "Lightweight",
+          value: ""
+        },
+        {
+          name: "Knockdown Quality",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV", type: "simple", effects: [
+        {
+          name: "Defensive Quality",
+          value: ""
+        },
+        {
+          name: "Customizable",
+          value: ""
+        },
+        {
+          name: "Pierce Quality",
+          value: ""
+        },
+        {
+          name: "Vicious Quality",
+          value: ""
+        },
+        {
+          name: "Stun Quality",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV", type: "intermediate", effects: [
+        {
+          name: "Ensnare Quality",
+          value: ""
+        },
+        {
+          name: "Deflection Quality",
+          value: ""
+        },
+        {
+          name: "Destructive",
+          value: ""
+        },
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV ADV", type: "advanced", effects: [
+        {
+          name: "Accurate Quality",
+          value: ""
+        },
+        {
+          name: "Lethal",
+          value: ""
+        },
+        {
+          name: "Sunder Quality",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "TRI TRI", type: "masterwork", effects: [
+        {
+          name: "Concussive Quality",
+          value: ""
+        },
+        {
+          name: "Integral Attachment",
+          value: ""
+        }
+      ]
+    }
+  ],
+  ranged: [
+    {
+      symbols: "ADV", type: "simple", effects: [
+        {
+          name: "Ion Quality",
+          value: ""
+        },
+        {
+          name: "Lightweight",
+          value: ""
+        },
+        {
+          name: "Disorient Quality",
+          value: ""
+        },
+        {
+          name: "Expanded Magazine",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV", type: "simple", effects: [
+        {
+          name: "Customizable",
+          value: ""
+        },
+        {
+          name: "Increased Range",
+          value: ""
+        },
+        {
+          name: "Knockdown Quality",
+          value: ""
+        },
+        {
+          name: "Vicious Quality",
+          value: ""
+        },
+        {
+          name: "Stun Setting",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV", type: "intermediate", effects: [
+        {
+          name: "Destructive",
+          value: ""
+        },
+        {
+          name: "Ensnare Quality",
+          value: ""
+        },
+        {
+          name: "Stun Quality",
+          value: ""
+        },
+        {
+          name: "Pierce Quality",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV ADV", type: "advanced", effects: [
+        {
+          name: "Auto-Fire Quality",
+          value: ""
+        },
+        {
+          name: "Burn Quality",
+          value: ""
+        },
+        {
+          name: "Lethal",
+          value: ""
+        },
+        {
+          name: "Accurate Quality",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "TRI TRI", type: "masterwork", effects: [
+        {
+          name: "Blast Quality",
+          value: ""
+        },
+        {
+          name: "Concussive Quality",
+          value: ""
+        },
+        {
+          name: "Integral Attachment",
+          value: ""
+        }
+      ]
+    }
+  ],
+  gadget: [
+    {
+      symbols: "ADV", type: "simple", effects: [
+        {
+          name: "Lightweight",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV", type: "simple", effects: [
+        {
+          name: "Compact",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV", type: "intermediate", effects: [
+        {
+          name: "Safety Features",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV ADV", type: "advanced", effects: [
+        {
+          name: "Inbuilt Weapon",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "TRI TRI", type: "masterwork", effects: [
+        {
+          name: "Supreme Craftsmanship",
+          value: ""
+        }
+      ]
+    }
+  ],
+  cybernetic: [
+    {
+      symbols: "ADV ADV", type: "simple", effects: [
+        {
+          name: "Integrated Tool",
+          value: ""
+        },
+        {
+          name: "Unobtrusive",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV", type: "intermediate", effects: [
+        {
+          name: "Tailored",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "ADV ADV ADV ADV", type: "advanced", effects: [
+        {
+          name: "Inbuilt Weapon",
+          value: ""
+        }
+      ]
+    },
+    {
+      symbols: "TRI TRI", type: "masterwork", effects: [
+        {
+          name: "Ion Shielded",
+          value: ""
+        }
+      ]
+    }
+  ]
+}
